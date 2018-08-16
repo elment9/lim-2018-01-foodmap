44a6bab0a166a2ae8cada32702d4e2fd74855131
elment9 <elizabethmendoza.unmsm@gmail.com>
2018-08-15 19:50:40 -0500

    Agregando fotografìas

--- src/js/data.js
+++ src/js/data.js
@@ -1,1 +1,42 @@
 // alert('data');
+// DOM
+//Splash 
+const body = document.getElementById('body');
+const splashContainer = document.getElementById('splash-container');
+const mainContainer = document.getElementById('main-container');
+//Contenedores
+const restaurantContainer = document.querySelector('.restaurants-container'); 
+//Inputs
+const inputSearch = document.getElementById('search-by-district');
+const searchButton = document.getElementById('search-button');
+
+
+// SPLASH
+mainContainer.style.display = 'none';
+
+const hideSplash = () => {
+    splashContainer.style.display = 'none';
+    mainContainer.style.display = 'block';
+    body.classList.remove('green');
+    body.classList.add('w3-light-grey', 'w3-content');
+    // body.classList.add(' ');
+}
+
+setTimeout(hideSplash, 2000);
+
+//MENU
+
+const w3_open = () => {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+const w3_close = () => {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+//FUNCIONALIDAD
+// searchButton.addEventListener('click', () => {
+//     searchByDiscrit = inputSearch.value;
+//     getData(searchByDiscrit)
+// })
